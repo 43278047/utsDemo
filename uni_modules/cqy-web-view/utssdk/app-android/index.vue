43f7a1fe71e9cbc8db84549baee15ae5a160c559
wanghao <43278047@qq.com>
2025-01-11 21:49:23 +0800

    完成webviwe的组件开发

--- uni_modules/cqy-web-view/utssdk/app-android/index.vue
+++ uni_modules/cqy-web-view/utssdk/app-android/index.vue
@@ -20,6 +20,11 @@
 	import URL from 'java.net.URL';
 	import Intent from 'android.content.Intent';
 	import Uri from 'android.net.Uri';
+	import WebBackForwardList from 'android.webkit.WebBackForwardList';
+	import WebHistoryItem from 'android.webkit.WebHistoryItem';
+	import WeakReference from 'java.lang.ref.WeakReference';
+	import Handler from 'android.os.Handler';
+	import Looper from 'android.os.Looper';
 
 	/**
 	 * 引入三方库
@@ -61,6 +66,17 @@
 			"hidden": {
 				type: Boolean,
 				default: true
+			},
+			/**
+			 * 前进或者后退的方法
+			 */
+			"back":{
+				type: Boolean,
+				default: true
+			},
+			"destructionWebView":{
+				type: Boolean,
+				default: true
 			}
 		},
 		/**
@@ -70,8 +86,8 @@
 			return {
 				// webView的UI实例,必须得用这个调用父组件的事件 $emit('')
 				webViewUIHandler: null as WebViewUIHandler | null,
-				// webView 实例对象,这个也可以直接使用 this.$el! 全局的函数
-				webView: null as WebView | null,
+				// webView 实例对象,这个也可以直接使用 this.$el! 全局的函数 有问题 不生效
+				webViewData: null as WebView | null,
 			}
 		},
 		/**
@@ -79,13 +95,17 @@
 		 */
 		watch: {
 			"webUrl": {
-				/**
-				 * 这里监听属性变化，并进行组件内部更新
-				 */
 				handler(newValue : string, oldValue : string) {
 					console.log("newValue " + newValue)
-					// console.log("oldValue "+oldValue)
 					if (newValue != oldValue) {
+						// UTSAndroid.getDispatcher("main").async(function(_){
+						// 	let url:string | null =  this.webViewData?.getUrl();
+						// 	if (url !=null  && newValue != url) {
+								console.log("赋值 newValue " + newValue)
+						// 		this.webViewData?.loadUrl(newValue);
+						// 	}
+						// },null)
+						
 						this.$el!.loadUrl(newValue);
 					}
 				},
@@ -108,7 +128,7 @@
 		 * 规则：如果没有配置expose，则methods中的方法均对外暴露，如果配置了expose，则以expose的配置为准向外暴露
 		 * ['publicMethod'] 含义为：只有 `publicMethod` 在实例上可用
 		 */
-		expose: ['doSomething', 'hideWebView', 'showWebView'],
+		expose: [ 'hideWebView', 'showWebView','back','forward','destructionWebView'],
 		// 这里面不支持回调函数
 		methods: {
 			/**
@@ -123,45 +143,32 @@
 			 * 2、(this.$refs["组件ref"] as UtsButtonElement).doSomething("uts-button");
 			 * 或 (uni.getElementById("组件id") as UtsButtonElement).doSomething("uts-button");
 			 */
-			doSomething(param : string) {
-
-			},
-			
-			hideWebView() {
-				// console.log("this.$el=", this.$el)
-				// console.log("this.$el=", this.webView)
-				this.$el!.visibility = View.GONE
-				// this.$el!.setVisibility(View.GONE);
-				// this.$el!.setVisibility(View.GONE);
-				// this.webView!.setVisibility(View.INVISIBLE);
-				// 隐藏且不占用空间
-				// this.$el!.setVisibility(View.GONE);
-			},
-			// 显示 WebView
-			// 这里 有问题 但是 "hidden" watch 就是好的,不知道为什么 showWebView 就是不行 一样的代码
-			showWebView() {
+			back(){
+				console.log("getBack")
+				UTSAndroid.getDispatcher("main").async(function(_){
+					this.webViewData?.goBack()
+				},null)
 				
-				this.$el!.visibility = View.VISIBLE
-				// this.$el!.loadUrl("https://www.baidu.com");
-				// // 确保父布局可见
-				// let parent : ViewGroup = this.$el!.getParent() as ViewGroup;
-				// if (parent.getVisibility() != View.VISIBLE) {
-				// 	parent.setVisibility(View.VISIBLE);
-				// }
-				// this.$el!.setVisibility(View.VISIBLE);
-				// // console.log("this.$el=",this.$el)
-				// // console.log("this.$el=",this.webView)
-				// // this.webView!.setVisibility(View.VISIBLE);
-				// // this.$el!.setVisibility(View.VISIBLE);
-				// // this.webView!.setVisibility(View.VISIBLE);
-				// this.$el!.loadUrl("https://www.baidu.com"); // 重新加载内容
 			},
-			/**
-			 * 内部使用的组件方法
-			 */
-			privateMethod() {
-
-			}
+			// 前进到下一个页面
+			forward(){
+				console.log("goForward")
+			UTSAndroid.getDispatcher("main").async(function(_){
+				this.webViewData?.goForward()
+			},null)
+			},
+			destructionWebView(){
+				console.log("destructionWebView")
+				UTSAndroid.getDispatcher("main").async(function(_){
+					this.webViewData?.destroy();
+				},null)
+			},
+			hideWebView() {
+				this.$el!.visibility = View.GONE
+			},
+			showWebView() {
+				this.$el!.visibility = View.VISIBLE
+			},
 		},
 		/**
 		 * [可选实现] 组件被创建，组件第一个生命周期，
@@ -169,7 +176,7 @@
 		 */
 		created() {
 			console.log("created")
-
+			
 		},
 		/**
 		 * [可选实现] 对应平台的view载体即将被创建，对应前端beforeMount
@@ -187,6 +194,7 @@
 		 */
 		NVLoad() : WebView {
 			let webView = new WebView($androidContext!);
+			this.webViewData = webView;
 			
 			const webSettings = webView.getSettings();
 			// 启用 JavaScript
@@ -196,25 +204,17 @@
 			// 缩放至屏幕宽度
 			webSettings.setLoadWithOverviewMode(true); 
 			
-			// 禁用硬件加速
-			// webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null); 
-			
 			// 请求定位权限
 			webSettings.setGeolocationEnabled(true);
 			
-			// // 启用混合内容加载（HTTP 和 HTTPS）
-			// if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			//     webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW); // 允许加载混合内容
-			// }
 			// 监听事件 
 			// 必须的 需要借助 UTSComponent 拿到webview的ui实例
 			const webViewUIHandler = new WebViewUIHandler(this);
 			webView.setWebViewClient(new MyWebViewClient(webViewUIHandler, webView));
 
-			webView.loadUrl(this.webUrl)
+			// webView.loadUrl(this.webUrl)
 			
 			this.webViewUIHandler = webViewUIHandler;
-			this.webView = webView;
 
 			return webView;
 		},
@@ -228,7 +228,7 @@
 		 * [可选实现] 原生View布局完成
 		 */
 		NVLayouted() {
-			console.log("NVLayouted", this.webUrl);
+			console.log("NVLayouted 原生View布局完成");
 			// 原生View布局完成 去调用父组件 告诉他 赋值 webUrl到这个组件上面来
 			this.webViewUIHandler?.setWebUrl();
 		},
@@ -288,6 +288,9 @@
 
 		private webView : WebView;
 		
+		// 是否执行过了 避免 onPageFinished 执行多次 
+		private  isPageFinishedExecuted :boolean = false; // 标志位
+
 
 		constructor(webViewUIHandler : WebViewUIHandler, webView : WebView) {
 			super();
@@ -298,34 +301,32 @@
 		// 页面加载完成时的逻辑 可能会触发多次 不知道为什么
 		override onPageFinished(view : WebView, url : string) : void {
 			super.onPageFinished(view, url);
-			// html: encodeURIComponent(document.documentElement.outerHTML), // 对 HTML 进行 URL 编码
-			view.evaluateJavascript(
-				`
-				(function() {
-				    const data = {
-				        title: document.title,
-				        host: window.location.host
-				    };
-				    return data;
-				})();
-				`,
-				new ValueCallbackMethod(this.webViewUIHandler)
-			);
+				this.isPageFinishedExecuted = true; // 设置标志位，确保只执行一次
+				// html: encodeURIComponent(document.documentElement.outerHTML), // 对 HTML 进行 URL 编码
+				view.evaluateJavascript(
+					`
+					(function() {
+					    const data = {
+					        title: document.title,
+					        host: window.location.host
+					    };
+					    return data;
+					})();
+					`,
+					new ValueCallbackMethod(this.webViewUIHandler)
+				);
 		}
 		
 		override shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): boolean {
 		        const url = request.getUrl().toString();
-				console.log("url===",url)
 				 // 拦截 HTTP 和 HTTPS 请求，让 WebView 正常加载
 				        if (request.url.scheme == "http" || request.url.scheme == "https") {
+							        // view.loadUrl(url) // 让 WebView 处理 HTTP/HTTPS 请求
 				            return false // 返回 false 表示让 WebView 正常加载
 				        }
 		        return this.handleUrlLoading(url,view);
 		    }
 		
-	// 	  override shouldOverrideUrlLoading(view: WebView, url: string): boolean {
-	// 	          return this.handleUrlLoading(url);
-	// 	      }
 	
 		 handleUrlLoading(url: string,view: WebView): boolean {
 		            // 拦截自定义协议
@@ -339,6 +340,7 @@
 				
 		    }
 	}
+
 
 	// 获取ui的实例 才能使用 $emit
 	class WebViewUIHandler {
@@ -355,11 +357,9 @@
 			});
 		}
 		// 告诉父组件 可以把url传给webVIew了
-
 		setWebUrl() {
 			this.comp.$emit("setWebUrl");
 		}
-
 
 	}
 </script>
